69036ff98c2c8a8a6403bb9d444e93ebe0c7d5cb
frederikkaempchen <frederikkaempchen@icloud.com>
2025-07-26 23:35:29 +0200

    improved DWA performance by reducing resolution

--- PathPlanning/DynamicWindowApproach/dynamic_window_approach.py
+++ PathPlanning/DynamicWindowApproach/dynamic_window_approach.py
@@ -38,17 +38,17 @@ class Config:
 
     def __init__(self):
         # robot parameter
-        self.max_speed = 1.0  # [m/s]
+        self.max_speed = 3.0  # [m/s]
         self.min_speed = -0.5  # [m/s]
         self.max_yaw_rate = 40.0 * math.pi / 180.0  # [rad/s]
         self.max_accel = 0.2  # [m/ss]
         self.max_delta_yaw_rate = 40.0 * math.pi / 180.0  # [rad/ss]
-        self.v_resolution = 0.01  # [m/s]
-        self.yaw_rate_resolution = 0.1 * math.pi / 180.0  # [rad/s]
+        self.v_resolution = 0.05  # [m/s]
+        self.yaw_rate_resolution = 0.5 * math.pi / 180.0  # [rad/s]
         self.dt = 0.1  # [s] Time tick for motion prediction
-        self.predict_time = 2.0  # [s]
+        self.predict_time = 3.0  # [s]
         self.to_goal_cost_gain = 0.4
-        self.speed_cost_gain = 5
+        self.speed_cost_gain = 7
         self.obstacle_cost_gain = 10.0
         self.turning_cost_gain = 0.5
         self.robot_stuck_flag_cons = 0.001  # constant to prevent robot stucked
